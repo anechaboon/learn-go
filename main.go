8a6487732cda2e156bdcd3eaff273eba651358a6
anechaBoon <82567055+anechaboon@users.noreply.github.com>
2025-12-09 09:04:31 +0700

    book

--- main.go
+++ main.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"strconv"
-
 	"github.com/gofiber/fiber/v2"
 )
 
-type Book struct {
-	ID     int `json:"id"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
-}
-
-var books []Book // this globally accessible slice will act as our in-memory database
 
 func main() {
 	app := fiber.New()
@@ -27,64 +18,4 @@ func main() {
 	app.Delete("/books/:id", deleteBook)
 
 	app.Listen(":8080")
-}
-
-func getBooks(c *fiber.Ctx) error {
-	return c.JSON(books)
-}
-
-func getBook(c *fiber.Ctx) error {
-	id, err := strconv.Atoi(c.Params("id"))
-	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString("Invalid book ID")
-	}
-	for _, book := range books {
-		if book.ID == id {
-			return c.JSON(book)
-		}
-	}
-	return c.Status(fiber.StatusNotFound).SendString("Book not found")
-}	
-
-func createBook(c *fiber.Ctx) error {
-	book := new(Book)
-	if err := c.BodyParser(book); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
-	book.ID = len(books) + 1
-	books = append(books, *book)
-	return c.Status(fiber.StatusCreated).JSON(book)
-}
-
-func updateBook(c *fiber.Ctx) error {
-	id, err := strconv.Atoi(c.Params("id"))
-	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString("Invalid book ID")
-	}
-	updatedBook := new(Book)
-	if err := c.BodyParser(updatedBook); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
-	for i, book := range books {
-		if book.ID == id {
-			books[i].Title = updatedBook.Title
-			books[i].Author = updatedBook.Author
-			return c.JSON(books[i])
-		}
-	}
-	return c.Status(fiber.StatusNotFound).SendString("Book not found")
-}
-
-func deleteBook(c *fiber.Ctx) error {
-	id, err := strconv.Atoi(c.Params("id"))
-	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString("Invalid book ID")
-	}
-	for i, book := range books {
-		if book.ID == id {
-			books = append(books[:i], books[i+1:]...)
-			return c.SendString("Book deleted")
-		}
-	}
-	return c.Status(fiber.StatusNotFound).SendString("Book not found")
 }
